4946726ba730d8e688dca36225b85b50fd9fc45a
Paula Sampaio <paulasampaio30ti@gmail.com>
2025-04-04 11:58:28 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <div className='fundo'>
+        {/* <div className='fundo'>
            <div className="comteiner">         
-           <div className="comteinerc">
-           <div className="cabesalho">
-           <div className="divImg">
-         <img className="logo" src="https://img.icons8.com/?size=100&id=rpU3BbTVADvt&format=png&color=000000"/>
-         </div>
-              <div>
-                 <p className="titulo">Pão De Mel</p>
-                </div>
+           <div className="comteinerc"> */}
+           <Header/>
+{/* 
             </div>
-
-            </div>
-          </div>
+          </div> */}
         
         
-         <div className="conteiner2">
+         {/* <div className="conteiner2">
             <div className="topo"> 
-             <p>Não e so Chocolate e Uma</p>
+             <p>Não é so Chocolate e Uma</p>
               <p>EXPERIENCIA</p>
               </div>
           </div>
 
          <div className="ali">
-            <div className="conteiner3 ">
+            <div className="conteiner3">
             
              <div className="gridItem caixa1 fontr">
                <p>DOCE DE LEITE </p>
              </div>
             
-             <div className="gridItem caixa2  fontr">
+             <div className="gridItem caixa2 fontr">
                <p>DOCE DE LEIE</p>
              </div>
             
-             <div className="gridItem  caixa3 fontr">
+             <div className="gridItem caixa3 fontr">
                 <p>BRIGADEIRO</p>
              </div>
             
@@ -54,10 +49,6 @@ function App() {
               <p>NINHO</p>
             </div>
             
-            <div className="gridItem caixa6 new">
-              <p className="new1">NEW</p>
-              <p className="cor">NINHO</p>
-            </div>
            
             <div className="gridItem  caixa7 fontr">
               <p className="cor">MUSE DE LIMAO</p>
@@ -70,23 +61,13 @@ function App() {
           </div>
 
         </div>
-        </div>
+        </div> <div className="gridItem caixa6 new">
+              <p className="new1">NEW</p>
+              <p className="cor">NINHO</p>
+            </div> */}
+           
 
-         <footer className="rodape">
-            <div className="roda">
-
-             <div className="divImgr">
-              <img className="logor" src="https://img.icons8.com/?size=100&id=El7pTE24c3iC&format=png&color=000000"/>
-              <img className="logor" src=" https://img.icons8.com/?size=100&id=FeFKy6pAkhxv&format=png&color=000000"/>
-              <img className="logor" src=" https://img.icons8.com/?size=100&id=21088&format=png&color=000000"/>
-              <img className="logor" src="https://img.icons8.com/?size=100&id=33029&format=png&color=000000"/>
-
-            </div>
-            <div><p>Pães de mel e cia MG/PR</p>
-                  <p>MG/ 9888898989</p>
-            </div>
-          </div>
-        </footer>
+         <Footer/>
         
         </>
 )}
