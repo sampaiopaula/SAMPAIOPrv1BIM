86d10b390825682857b75368e54384561fa37e7b
Paula Sampaio <paulasampaio30ti@gmail.com>
2025-04-21 12:58:23 -0300

    componentes

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import './Footer.css'
+import './Banner.css'
 
-function Footer() {
+function Banner() {
   return (
     <>
-      
+      <div className="divImg">
+                <img className="logo" src="https://img.icons8.com/?size=100&id=rpU3BbTVADvt&format=png&color=000000" />
+                </div>
     </>
   )
 }
-export default Footer
+export default Banner
 
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './ProductList.css'
+
+function ProductList() {
+  return (
+    <>
+    <div className="ali">
+            <div className="conteiner3">
+            
+             <div className="gridItem caixa1 fontr">
+               <p>DOCE DE LEITE</p>
+             </div>
+            
+             <div className="gridItem caixa2 fontr">
+               <p>DOCE DE LEiTE</p>
+             </div>
+            
+             <div className="gridItem caixa3 fontr">
+                <p>BRIGADEIRO</p>
+             </div>
+            
+               <div className="gridItem caixa4 fontr">
+              <p>BRIGADEIRO</p>
+               </div>
+           
+              <div className="gridItem caixa5 fontr">
+              <p>NINHO</p>
+            </div>
+            
+           
+            <div className="gridItem  caixa7 fontr">
+              <p className="cor">MUSE DE LIMAO</p>
+            </div>
+            
+             <div className="gridItem caixa8 new"> 
+               <p className="new1">NEW</p>
+               <p className="cor">MUSE DE LIMAO</p>
+              </div>
+
+              <div className="gridItem caixa6 new">
+                <p className="new1">NEW</p>
+                <p className="cor">NINHO</p>
+            </div>
+          </div>
+
+        </div>
+        
+    </>
+  )
+}
+export default ProductList
+
